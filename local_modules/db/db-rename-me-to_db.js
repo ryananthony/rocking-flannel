d5741f3f5ee227d5b69c5f67480422231a493303
Ryan Richardson <ryananthonyrichardson@gmail.com>
2013-05-12 22:30:24 -0700

    Removed unused DB Module

--- local_modules/db/db-rename-me-to_db.js
+++
@@ -1,6 +0,0 @@
-var cradle = require('cradle');
-var db = new(cradle.Connection)(/*couchdb host URL (as String)*/, /* port (as INT) */, {
-  auth: { username: /*couchdb user (as String)*/, password: /*couchdb password (as String)*/ }
-}).database(/*database name (as String)*/);
-
-module.exports = db;
